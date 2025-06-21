1dcc75dffb938e787353f6c0dd7921cff4e0c7e0
adarshm0602 <adarshm@MacBook-Air.local>
2025-06-21 23:17:01 +0530

    data types

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public static void main(String args[]){
+        System.out.println("There are 8 data types in java");
+        byte b = 10;
+        char ch = 'a';
+        boolean var = true;
+        float price = 10.5f;
+        int num = 25;
+        long big = 100000000;
+        double BIG = 100.234345;
+        System.out.println(b);
+        System.out.println(ch);
+        System.out.println(var);
+        System.out.println(price);
+        System.out.println(num);
+        System.out.println(big);
+        System.out.println(BIG);
+    }
+}
