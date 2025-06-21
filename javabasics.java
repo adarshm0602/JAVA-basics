de66294f58c6168b1db81c7b10a567072aa698fb
adarshm0602 <adarshm@MacBook-Air.local>
2025-06-21 22:27:32 +0530

    first commit

---
+++ javabasics.java
@@ -0,0 +1,8 @@
+
+public class javabasics {
+    public static void main(String args[]){
+        System.out.println("Hello world!");
+        System.out.println("This is my first Java program");
+        System.out.println("I am very excited to learn java!");
+    }
+}
