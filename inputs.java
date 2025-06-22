5e498b252ed15df75c4fcefc5a259613aecdbb0c
adarshm0602 <adarshm@MacBook-Air.local>
2025-06-22 12:55:12 +0530

    yello

--- inputs.java
+++ inputs.java
@@ -3,7 +3,7 @@ public class inputs {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
-        System.out.println(name);
+        System.out.println("My name is:"+name);
         Boolean B = sc.nextBoolean();
         System.out.println(B);
 
